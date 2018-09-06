2befb1d4dfba10d446ce226e33ccc45fdb42e428
Ania <koziolek1988@gmail.com>
2018-09-06 18:24:28 +0200

    commit zadanka

--- program/main.py
+++ program/main.py
@@ -1,1 +1,19 @@
 # nowy program
+imie = input("podaj imię: ")
+wiek = input("ile masz lat?: ")
+plec = input("podaj płeć K lub M: ")
+nazwa_plci = ""
+
+if plec == "K":
+    nazwa_plci = "kobietą"
+    print (f"masz na imię: {imie}, masz {wiek} lat i jesteś {nazwa_plci}")
+
+elif plec == "M":
+    nazwa_plci = "mężczyzną"
+    print(f"masz na imię: {imie}, masz {wiek} lat i jesteś {nazwa_plci}")
+
+else:
+    print ("błędny symbol płci")
+
+
+
